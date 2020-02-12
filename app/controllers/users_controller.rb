531726f050518362f9f25761bb95ed53a86af3b2
mani21na <mani21na@gmail.com>
2020-02-12 14:53:02 -0800

    delete unnecessary files

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  before_action :require_login
+  skip_before_action :require_login, only: [:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+    @user = User.find[params[:id]]
+  end
+
+  private
+ 
+  def require_login
+    return head(:forbidden) unless session.include? :user_id
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get “/login”, to: “sessions#new”
-  post “/login”, to: “sessions#create”
-
-  resources :user
+  resources :users
     
-  resources :list, only: [:new, :create, :edit, :upgrade, :index, :destroy] do
-    resources :share_user, only: [:create, :destroy, :index]
+  resources :lists, only: [:new, :create, :edit, :upgrade, :index, :destroy] do
+    resources :share_users, only: [:create, :destroy, :index]
   end
 
-  resources :list 
-  resources :share_user
+  resources :lists
+  resources :share_users
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 end
