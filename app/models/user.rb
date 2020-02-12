07ea47fbc7e9a25d959fcf954e9d872f480c6485
mani21na <mani21na@gmail.com>
2020-02-12 15:05:35 -0800

    set validates helpers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_many :lists 
     has_many :share_users, through: :lists
+
+    validates :username, :password, :first_name, :last_name, presence: true
+    validates :username, uniqueness: true
+    validates :username, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
