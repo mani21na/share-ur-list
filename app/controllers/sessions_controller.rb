a51f43eedccd44b4a028712e58bc951f6ae460f7
mani21na <mani21na@gmail.com>
2020-02-12 18:47:12 -0800

    set sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,14 @@ class SessionsController < ApplicationController
     end
  
     def create
-        session[:username] = params[:username]
+        @user = User.find_by(:username => params[:username])
+        session[:user_id] = @user.id
 
-        redirect_to user_path
+        redirect_to user_path(@user)
     end
 
     def destroy
-        session.delete :username
+        session.destroy
+        redirect_to '/'
     end
 end
