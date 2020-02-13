775613039ab0e953348c6bf8b48ce3537795b933
mani21na <mani21na@gmail.com>
2020-02-12 20:21:08 -0800

    change path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,13 @@ class SessionsController < ApplicationController
         @user = User.find_by(:username => params[:username])
         session[:user_id] = @user.id
 
+        #binding.pry
+
         redirect_to user_path(@user)
     end
 
     def destroy
         session.destroy
-        redirect_to '/'
+        redirect_to root_path
     end
 end
