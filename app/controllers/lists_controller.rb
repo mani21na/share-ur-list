1806ebd988a9ad1e35ad856def65a35024ab9764
mani21na <mani21na@gmail.com>
2020-02-14 07:39:12 -0800

    fix nested attributes

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,41 +1,46 @@
 class ListsController < ApplicationController
   before_action :set_list, only: [:edit, :update, :destroy, :show]
   before_action :set_user_id
-  before_action :set_nested_attributes, only: [:new, :create, :show, :edit, :update]
-  before_action :set_items, only: [:show, :edit]
   before_action :require_login
 
   def new
     @list = List.new
+    @num = 0
+    10.times do
+      @num += 1
+      @list.items.build(item_no: "#{@num}")
+    end
   end
 
   def create
     @list = List.new(list_params)
     if @list.valid?
       @list.save
-      redirect_to lists_path
-    else 
+      redirect_to user_path(current_user.id)
+    else
       render :new
     end
   end
 
-  def edit
-  end
+  #def edit
+  #end
 
   def update
+    if @list.update(list_params)
+      redirect_to user_path(current_user.id)
+    else 
+       render :edit
+    end
   end
 
   def destroy
+    #binding.pry
+    @list.items.destroy
+    @list.destroy
+    redirect_to user_path(current_user.id)
   end
 
-  #error! => items are not showing
   def show
-    @num = 0
-    @items  = set_list.items
-    10.times do
-      @num =+ 1
-      @list.items.build(item_no: "#{@num}")
-    end
   end
 
   #def index
@@ -53,18 +58,6 @@ class ListsController < ApplicationController
   end
 
   def list_params
-    params.require(:list).permit(:user_id, :subject, item_no_attributes: [:item, :quantity, :done])
+    params.require(:list).permit(:user_id, :subject, items_attributes: [:id, :item_no, :item, :quantity, :done])
   end 
-
-  def set_nested_attributes
-    @num = 0
-    10.times do
-      @num =+ 1
-      @list.items.build(item_no: "#{@num}")
-    end
-  end
-
-  def set_items
-    @items  = set_list.items
-  end
 end
