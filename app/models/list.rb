62de9a6c1ade488291c87a0dfe7857e4e551f042
mani21na <mani21na@gmail.com>
2020-02-13 22:20:40 -0800

    set nested items attributes

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,9 @@
 class List < ApplicationRecord
     belongs_to :user
+    has_many :items
     has_many :share_users
-    accepts_nested_attributes_for :list
 
-    validates :list, presence: true
+    accepts_nested_attributes_for :items
+
+    validates :subject, presence: true, uniqueness: true
 end
