0feefec2e8c05f68a36c723f27e199f7f8e3254d
mani21na <mani21na@gmail.com>
2020-02-14 00:05:55 -0800

    add before actions

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,12 @@
 class ListsController < ApplicationController
   before_action :set_list, only: [:edit, :update, :destroy, :show]
+  before_action :set_user_id
+  before_action :set_nested_attributes, only: [:new, :create, :show, :edit, :update]
+  before_action :set_items, only: [:show, :edit]
   before_action :require_login
 
   def new
     @list = List.new
-    @user_id = current_user.id
-    @num = 0
-    10.times do
-      @num =+ 1
-      @list.items.build(item_no: "#{@num}")
-    end
   end
 
   def create
@@ -18,12 +15,6 @@ class ListsController < ApplicationController
       @list.save
       redirect_to lists_path
     else 
-      @user_id = current_user.id
-      @num = 0
-      10.times do
-        @num =+ 1
-        @list.items.build(item_no: "#{@num}")
-      end
       render :new
     end
   end
@@ -37,21 +28,43 @@ class ListsController < ApplicationController
   def destroy
   end
 
+  #error! => items are not showing
   def show
+    @num = 0
+    @items  = set_list.items
+    10.times do
+      @num =+ 1
+      @list.items.build(item_no: "#{@num}")
+    end
   end
 
-  def index
-    @list = List.all.find_all{ |list| list.find_by(:user_id => params[:id]) }
-binding.pry
-  end
+  #def index
+  #  @lists = List.all.find_all{ |list| list.user_id == current_user.id }
+  #end
 
   private 
 
   def set_list
     @list = List.find(params[:id])
-  end 
+  end
+  
+  def set_user_id
+    @user_id = current_user.id
+  end
 
   def list_params
     params.require(:list).permit(:user_id, :subject, item_no_attributes: [:item, :quantity, :done])
   end 
+
+  def set_nested_attributes
+    @num = 0
+    10.times do
+      @num =+ 1
+      @list.items.build(item_no: "#{@num}")
+    end
+  end
+
+  def set_items
+    @items  = set_list.items
+  end
 end
