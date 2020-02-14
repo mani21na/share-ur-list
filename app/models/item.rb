f4c0c509ef6ab66723be63c17db8e34cacfe2714
mani21na <mani21na@gmail.com>
2020-02-14 07:40:10 -0800

    set validates

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
     belongs_to :list
+    validates :item, presence: true, if: :done_checked?
+    validates :quantity, presence: true, if: :item_is_exist?
+
+    def item_is_exist?
+        !(item == "") 
+    end
+
+    def done_checked?
+        !!(done == "1")
+    end
 end
