87422ef1714807307e64f97fb932411642aff438
mani21na <mani21na@gmail.com>
2020-02-13 22:19:16 -0800

    add new and create methods

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,26 +3,27 @@ class ListsController < ApplicationController
   before_action :require_login
 
   def new
-    binding.pry
     @list = List.new
+    @user_id = current_user.id
     @num = 0
-    5.times do
-      num =+ 1
-      @list.list.build(name_num: "#{num}")
+    10.times do
+      @num =+ 1
+      @list.items.build(item_no: "#{@num}")
     end
   end
 
   def create
-    @list = List.new
-    @num = 0
-    5.times do
-      num =+ 1
-      @list.list.build(name_num: "#{num}")
-    end
+    @list = List.new(list_params)
     if @list.valid?
       @list.save
       redirect_to lists_path
     else 
+      @user_id = current_user.id
+      @num = 0
+      10.times do
+        @num =+ 1
+        @list.items.build(item_no: "#{@num}")
+      end
       render :new
     end
   end
@@ -41,6 +42,7 @@ class ListsController < ApplicationController
 
   def index
     @list = List.all.find_all{ |list| list.find_by(:user_id => params[:id]) }
+binding.pry
   end
 
   private 
@@ -50,6 +52,6 @@ class ListsController < ApplicationController
   end 
 
   def list_params
-    params.require(:list).permit(:subject, name_num: [:item, :done])
+    params.require(:list).permit(:user_id, :subject, item_no_attributes: [:item, :quantity, :done])
   end 
 end
