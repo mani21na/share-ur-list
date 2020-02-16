dd8108472fd7d247597bb7c32eb8d952b110fac7
mani21na <mani21na@gmail.com>
2020-02-15 21:52:52 -0800

    set secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :lists 
     has_many :share_users, through: :lists
 
     validates :username, :password, :first_name, :last_name, presence: true
     validates :username, uniqueness: true
     validates :username, format: { with: URI::MailTo::EMAIL_REGEXP } 
+
+    def self.search_users_by_username(search)
+        @users = User.select("username, id").where("username like ?", "%#{search}%")
+    end
 end
