42450e096a6c052834a0285053e66061dc5070a2
mani21na <mani21na@gmail.com>
2020-02-15 21:50:29 -0800

    add security password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,9 @@ class SessionsController < ApplicationController
     end
  
     def create
-        @user = User.find_by(:username => params[:username])
+        @user = User.find_by(username: params[:username])
+        return head(:forbidden) unless @user.authenticate(params[:password])
         session[:user_id] = @user.id
-
-        #binding.pry
 
         redirect_to user_path(@user)
     end
