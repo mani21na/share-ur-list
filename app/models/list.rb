a8515ade4a999c32f24c616d5088aadd8419d4a0
mani21na <mani21na@gmail.com>
2020-02-15 21:52:14 -0800

    fix validates for nested attributes

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,12 @@
 class List < ApplicationRecord
     belongs_to :user
-    has_many :items
+    has_many :items, :dependent => :delete_all
     has_many :share_users
 
-    accepts_nested_attributes_for :items, :allow_destroy => true
 
-    validates :subject, presence: true, uniqueness: true
+    accepts_nested_attributes_for :items, :allow_destroy => true
+    validates_associated :items
+
+    validates :subject, presence: true
+    validates_uniqueness_of :subject, scope: :user_id
 end
